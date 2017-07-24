8a216079c7b4f0c46b37f40f0c884b5b53c92b73
mgbrando <mgbrando29@gmail.com>
2017-07-23 20:22:39 -0400

    changed to remote database

--- server/config.js
+++ server/config.js
@@ -1,4 +1,5 @@
 module.exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
-                      'mongodb://localhost/gwhighcommand';
+                       'mongodb://mgbrando:aidynmb9@ds149030.mlab.com:49030/gwhighcommand';
+                       //'mongodb://localhost/gwhighcommand';
 module.exports.PORT = process.env.PORT || 3001;
